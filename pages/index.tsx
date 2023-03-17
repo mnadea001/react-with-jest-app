900b6f1f5385e30c8faa85bfef33def472cc0527
Mathilde <mathilde.nadeau@hotmail.fr>
2023-03-17 11:18:39 +0100

    style

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,19 @@ export default function Home() {
 
   const [number1, setNumber1] = useState(0);
   const [number2, setNumber2] = useState(0);
-const add = () => {
-  setResult(parseInt(number1) + parseInt(number2))
-};
-const substract = () => {
-  setResult(parseInt(number1) - parseInt(number2))
-};
-const multiply = () => {
-  setResult(parseInt(number1) * parseInt(number2))
-};
-const divide = () => {
-  setResult(parseInt(number1) / parseInt(number2))
-};
-const [result, setResult] = useState(0);
+  const add = () => {
+    setResult(parseInt(number1) + parseInt(number2))
+  };
+  const substract = () => {
+    setResult(parseInt(number1) - parseInt(number2))
+  };
+  const multiply = () => {
+    setResult(parseInt(number1) * parseInt(number2))
+  };
+  const divide = () => {
+    setResult(parseInt(number1) / parseInt(number2))
+  };
+  const [result, setResult] = useState(0);
 
   return (
     <div className={styles.container}>
@@ -29,7 +29,7 @@ const [result, setResult] = useState(0);
       </Head>
 
       <main>
-        <div className={styles.result} data-testid="result">RESULT = {result}</div>
+        <div className={styles.result} data-testid="result">Result:  {result}</div>
         <input type="number" className={styles.input} data-testid={number1} value={number1} onChange={(e) => setNumber1(e.target.value)} />
 
         <input type="number" className={styles.input} data-testid={number2} value={number2} onChange={(e) => setNumber2(e.target.value)} />
